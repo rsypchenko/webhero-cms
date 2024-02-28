7082675a516960508facd3dfa686d129f283fbee
imhotep <r.sipchenko@gmail.com>
2024-02-27 21:04:25 -0700

    s3 bucket integration

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,34 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'webhero-s3-bucket.s3.us-east-2.amazonaws.com', // change here
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'webhero-s3-bucket.s3.us-east-2.amazonaws.com', // change here
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
